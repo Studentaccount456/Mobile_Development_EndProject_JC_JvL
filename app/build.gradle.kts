358108ffa66f657e25f97170c4d11e003b6936c6
Studentaccount456 <S127806@ap.be>
2023-11-22 20:54:54 +0100

    Add RegisterScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.navigation:navigation-compose:2.4.0-alpha08")
     implementation ("androidx.navigation:navigation-compose:2.3.5")
+    implementation("com.google.android.material:material:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/mobile_dev_endproject_jc_jvl/RegisterActivity.kt
+++ app/src/main/java/com/example/mobile_dev_endproject_jc_jvl/RegisterActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mobile_dev_endproject_jc_jvl
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -10,65 +9,66 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
-    private lateinit var forgotPasswordTextView: TextView
-    private lateinit var loginButton: Button
-    private lateinit var registerButton: Button
+    private lateinit var usernameEditText: EditText
+    private lateinit var submitButton: Button
+    private lateinit var returnButton: Button
 
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
 
         auth = FirebaseAuth.getInstance()
 
-        emailEditText = findViewById(R.id.emailEditText)
-        passwordEditText = findViewById(R.id.passwordEditText)
-        forgotPasswordTextView = findViewById(R.id.forgotPasswordTextView)
-        loginButton = findViewById(R.id.loginButton)
-        registerButton = findViewById(R.id.registerButton)
+        emailEditText = findViewById(R.id.emailRegisterEditText)
+        passwordEditText = findViewById(R.id.passwordRegisterEditText)
+        usernameEditText = findViewById(R.id.usernameEditText)
+        submitButton = findViewById(R.id.submitButton)
+        returnButton = findViewById(R.id.returnButton)
 
-        loginButton.setOnClickListener {
+        submitButton.setOnClickListener {
             val email = emailEditText.text.toString().trim()
             val password = passwordEditText.text.toString().trim()
+            val username = usernameEditText.text.toString().trim()
 
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                loginUser(email, password)
+            if (email.isNotEmpty() && password.isNotEmpty() && username.isNotEmpty()) {
+                registerUser(email, password)
+                finish()
             } else {
                 // Handle empty fields
+                showSnackbar("Please fill in all fields.")
             }
         }
 
-        forgotPasswordTextView.setOnClickListener {
-            // Handle forgot password
-        }
-
-        registerButton.setOnClickListener {
-            // To Registerscreen
-            startActivity(Intent(this, RegisterActivity::class.java))
+        returnButton.setOnClickListener {
+            // Return to LoginActivity
+            finish() // Close the RegisterActivity and return to the LoginActivity
         }
     }
 
-    private fun loginUser(email: String, password: String) {
-        auth.signInWithEmailAndPassword(email, password)
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Login successful
+                    // Registration successful
                     // You can navigate to the main activity or perform other actions
+                    showSnackbar("Registration successful!")
                 } else {
-                    // If login fails, display a message to the user.
+                    // If registration fails, display a message to the user.
                     // You can customize the error message based on the task.exception
                     // For example, task.exception?.message
-                    // Handle authentication errors
+                    // Handle registration errors
+                    val errorMessage = task.exception?.message ?: "Registration failed"
+                    showSnackbar(errorMessage)
                 }
-                val errorMessage = task.exception?.message ?: "Login failed"
-                showSnackbar(errorMessage)
             }
     }
+
     private fun showSnackbar(message: String) {
         // Assuming your root view is a CoordinatorLayout, replace it with the appropriate view type if needed
         val rootView = findViewById<View>(android.R.id.content)
