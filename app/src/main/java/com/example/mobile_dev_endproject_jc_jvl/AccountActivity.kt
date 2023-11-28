c0d793279403f90e9a9a7e3b55c59c2cecc15d17
Studentaccount456 <S127806@ap.be>
2023-11-28 20:59:21 +0100

    Finalize base AccountActivity

--- app/src/main/java/com/example/mobile_dev_endproject_jc_jvl/AccountActivity.kt
+++ app/src/main/java/com/example/mobile_dev_endproject_jc_jvl/AccountActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mobile_dev_endproject_jc_jvl
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlin.math.log
 
 class AccountActivity : AppCompatActivity(){
 
@@ -53,34 +55,93 @@ class AccountActivity : AppCompatActivity(){
             val userRef = db.collection("ThePlayers").document(userId)
             userRef.get().addOnSuccessListener { document ->
                 if (document != null && document.exists()) {
-                    // Get profile details
-                    val profileDetails = document.get("TheProfileDetails") as Map<String, Any>
-                    val avatarUrl = profileDetails["Avatar"] as String
-                    val nickname = profileDetails["Nickname"] as String
-                    val playLocation = profileDetails["PrefferedPlayLocation"] as String
+                    Log.d("AccountActivity", "1) Document data: ${document.data}")
 
-                    // Get preferences
-                    val preferences =
-                        document.get("ThePreferencesPlayer") as Map<String, Any>
-                    val typeMatch = preferences["PrefferedTypeMatch"] as String
-                    val handPlay = preferences["PrefferedHandPlay"] as String
-                    val timeToPlay = preferences["PrefferedTimeToPlay"] as String
-                    val courtPosition = preferences["PrefferedCourtPosition"] as String
-                    val genderToPlayAgainst = preferences["PrefferedGenderToPlayAgainst"] as String
+                    // Retrieve the "TheProfileDetails" subcollection
+                    val profileDetailsRef = userRef.collection("TheProfileDetails")
+                    profileDetailsRef.get().addOnSuccessListener { profileDetailsSnapshot ->
+                        for (profileDetailsDocument in profileDetailsSnapshot.documents) {
+                            // Now you have access to each document in the "TheProfileDetails" subcollection
+                            val profileDetailsData = profileDetailsDocument.data
+                            Log.d("AccountActivity", "2) Profile Details data: $profileDetailsData")
 
-                    // Update UI with fetched data
-                    Glide.with(this).load(avatarUrl).into(profileImage)
-                    nicknameText.text = nickname
-                    locationText.text = playLocation
-                    typeMatchText.text = typeMatch
-                    handPlayText.text = handPlay
-                    timeToPlayText.text = timeToPlay
-                    courtPositionText.text = courtPosition
-                    genderToPlayAgainstText.text = genderToPlayAgainst
+                            if (profileDetailsData != null) {
+                                // Extract fields
+                                val levelInformation = profileDetailsData["Level"]?.toString()?.toInt()
+                                Log.d("AccountActivity", "2.1) Profile Details data: $levelInformation")
+                                val followersInformation = profileDetailsData["Followers"]?.toString()?.toInt()
+                                Log.d("AccountActivity", "2.2) Profile Details data: $followersInformation")
+                                val followingInformation = profileDetailsData["Following"]?.toString()?.toInt()
+                                Log.d("AccountActivity", "2.3) Profile Details data: $followingInformation")
+                                val avatarUrl = profileDetailsData["Avatar"] as? String
+                                val nickname = profileDetailsData["Nickname"] as? String
+                                Log.d("AccountActivity", "2.0) Profile Details data: $nickname")
+                                Log.d(
+                                    "AccountActivity",
+                                    "Avatar URL: $avatarUrl, Nickname: $nickname, Level: $levelInformation, followers: $followersInformation, following: $followingInformation "
+                                )
+
+                                // Check if any of the required fields is null before updating UI
+                                if (avatarUrl != null && nickname != null && levelInformation != null && followersInformation != null && followingInformation != null) {
+                                    // Update UI with fetched data
+                                    Glide.with(this).load(avatarUrl).into(profileImage)
+                                    nicknameText.text = nickname
+                                    followersText.text = "Followers: " + followersInformation.toString()
+                                    followingText.text = "Following: " + followingInformation.toString()
+                                    levelText.text = "Level: " + levelInformation.toString()
+
+
+                                    // Fetch "ThePreferencesPlayer" subcollection
+                                    val preferencesRef = userRef.collection("ThePreferencesPlayer")
+                                    preferencesRef.get().addOnSuccessListener { preferencesSnapshot ->
+                                        for (preferencesDocument in preferencesSnapshot.documents) {
+                                            // Now you have access to each document in the "ThePreferencesPlayer" subcollection
+                                            val preferencesData = preferencesDocument.data
+                                            Log.d("AccountActivity", "3) Preferences data: $preferencesData")
+
+                                            // Extract fields from the "ThePreferencesPlayer" document
+                                            val typeMatch = preferencesData?.get("PrefferedTypeMatch") as? String
+                                            val handPlay = preferencesData?.get("PrefferedHandPlay") as? String
+                                            val timeToPlay = preferencesData?.get("PrefferedTimeToPlay") as? String
+                                            val courtPosition = preferencesData?.get("PrefferedCourtPosition") as? String
+                                            val genderToPlayAgainst =
+                                                preferencesData?.get("PrefferedGenderToPlayAgainst") as? String
+                                            val playLocation = preferencesData?.get("PrefferedPlayLocation") as? String
+
+                                            // Check if any of the required preference fields is null before updating UI
+                                            if (typeMatch != null && handPlay != null && timeToPlay != null
+                                                && courtPosition != null && genderToPlayAgainst != null && playLocation != null
+                                            ) {
+                                                // Update UI with fetched preferences
+                                                locationText.text = playLocation
+                                                typeMatchText.text = "Type Match: " + typeMatch
+                                                handPlayText.text = "Preferred Hand: " + handPlay
+                                                timeToPlayText.text = "Preffered Time: " + timeToPlay
+                                                courtPositionText.text = "Preffered Court Position: " + courtPosition
+                                                genderToPlayAgainstText.text = "Preffered Gender to play against: " + genderToPlayAgainst
+                                            } else {
+                                                // Handle the case where some preference fields are null
+                                                // Show an error message or take appropriate action
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    // Handle the case where some fields in "Nickname" document are null
+                                    // Show an error message or take appropriate action
+                                }
+                            } else {
+                                // Handle the case where "Nickname" document is null
+                                // Show an error message or take appropriate action
+                            }
+                        }
+                    }
+
+                    // Continue with the rest of your code...
                 }
             }
         }
 
+
         // Set up the rest of your UI and handle button clicks as needed
-    }
-}
+            }
+        }
--- app/src/main/java/com/example/mobile_dev_endproject_jc_jvl/LoginActivity.kt
+++ app/src/main/java/com/example/mobile_dev_endproject_jc_jvl/LoginActivity.kt
@@ -59,6 +59,7 @@ class LoginActivity : AppCompatActivity() {
                 if (task.isSuccessful) {
                     // Login successful
                     // You can navigate to the main activity or perform other actions
+                    startActivity(Intent(this, AccountActivity::class.java))
                 } else {
                     // If login fails, display a message to the user.
                     // You can customize the error message based on the task.exception
